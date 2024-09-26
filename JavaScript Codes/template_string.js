54dcf6598a95c0537348ac9e4d457db2826f0cba
Sneha Lohana <sneha.lohana16@gmail.com>
2024-09-26 15:45:18 +0530

    1. String Templates

---
+++ JavaScript Codes/template_string.js
@@ -0,0 +1,11 @@
+const username = "Sneha";
+    // Template String
+    const str3 = `
+    <div>
+        Hello ${username}
+        ${4+5}
+        ${Math.random()}
+    </div>
+    `;
+
+    console.log(str3);
