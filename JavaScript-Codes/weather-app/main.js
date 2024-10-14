80df06693399fd9b7afb4e5ee63a422608a6da2d
Sneha Lohana <sneha.lohana16@gmail.com>
2024-10-14 16:24:01 +0530

    Weather app with geolocation

--- JavaScript-Codes/weather-app/main.js
+++ JavaScript-Codes/weather-app/main.js
@@ -3,6 +3,8 @@ const days = ["Sun", "Mon", "Tue", "Wed", "Thur", "Fri", "Sat"];
 const dates = [0, "1st", "2nd", "3rd", "4th", "5th", "6th", "7th", "8th", "9th", "10th", "11th", "12th", "13th", "14th", "15th", "16th", "17th", "18th", "19th", "20th", "21st", "22nd", "23rd", "24th", "25th", "26th", "27th", "28th", "29th", "30th", "31st"];
 const months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
 
+const forecast = document.querySelector(".hourly-forecast");
+
 // ApI key to retrieve data
 const API_key = "427f3c11a0444fc5c1014874c61d90df";
 
@@ -32,32 +34,63 @@ function setCurrentWeatherData(data){
     document.querySelector("#wind").innerHTML = data.wind.speed + "Km/h";
 }
 
-function getCurrentWeatherData(city){
+function getCurrentWeatherData(city, lat, lon){
     let url;
-    url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_key}&units=metric`;
+    if (city){
+      url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_key}&units=metric`;
+    } else {
+      url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_key}&units=metric`;
+    }
+    
     fetch(url)
         .then(res => res.json())
         .then(data => setCurrentWeatherData(data))
         .catch(error => console.error(error));
 }
 
-const forecast = document.querySelector(".hourly-forecast");
-
 function setForecastWeatherData(data){
-    console.log(data)
-    let wd = data.list[0];
+  forecast.innerHTML = "";
+  data.list.forEach(wd => {
+    // let wd = data.list[0];
+    // console.log(wd);
     const forecastCard = document.createElement('div');
     forecastCard.className = 'col forecast-card';
 
+    // Adding time to the card
     const forecastTime = document.createElement('h3');
     forecastTime.textContent = formatTime(new Date(wd.dt_txt));
-    forecast.append(forecastTime);
-    forecast.append(document.createElement('br'));
+    forecastCard.append(forecastTime);
+    forecastCard.append(document.createElement('br'));
+
+    // Adding the image of the forecast to the card
+    const forecastIcon = document.createElement('img');
+    forecastIcon.src = `https://openweathermap.org/img/wn/${wd.weather[0].icon}@2x.png`;
+    forecastCard.append(forecastIcon);
+
+    // Adding the forecast description to the card
+    const forecastDesc = document.createElement('p');
+    forecastDesc.className = "weather";
+    forecastDesc.textContent = wd.weather[0].description;
+    forecastDesc.setAttribute("style", "text-transform: capitalize;");
+    forecastCard.append(forecastDesc);
+
+    // Adding the forecast temperature to the card
+    const forecastTemp = document.createElement('span');
+    forecastTemp.innerHTML = `${Math.round(wd.main.temp)}&deg;C`;
+    forecastCard.append(forecastTemp);
+
+    forecast.append(forecastCard);
+  });
 }
 
-function getForecastWeatherData(city){
+function getForecastWeatherData(city, lat, lon){
     let url;
-    url = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${API_key}&units=metric&cnt=5`;
+    if (city){
+      url = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${API_key}&units=metric&cnt=5`;
+    } else{
+      url = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${API_key}&units=metric&cnt=5`;
+    }
+    
     fetch(url)
         .then(res => res.json())
         .then(data => setForecastWeatherData(data))
@@ -74,5 +107,14 @@ function handleSubmit(event) {
 const searchBar = document.querySelector("#search-form");
 searchBar.addEventListener("submit", handleSubmit);
 
-getCurrentWeatherData("Pune");
-getForecastWeatherData("Pune");
+// Loading the geo-location using navigator
+navigator.geolocation.getCurrentPosition((position) => {
+  console.log(position);
+  getCurrentWeatherData(null, position.coords.latitude, position.coords.longitude);
+  getForecastWeatherData(null, position.coords.latitude, position.coords.longitude);
+}, error => {
+  console.error(error);
+  getCurrentWeatherData("Pune");
+  getForecastWeatherData("Pune");
+});
+
