6c43fc9e8e9a46f4694d596022938aa8fcc4abde
Sneha Lohana <sneha.lohana16@gmail.com>
2024-12-07 13:39:07 +0530

    JSX

--- React-JS/React-Projects/my-first-app/src/index.js
+++ React-JS/React-Projects/my-first-app/src/index.js
@@ -4,14 +4,40 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// // If you want to start measuring performance in your app, pass a function
+// // to log results (for example: reportWebVitals(console.log))
+// // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// reportWebVitals();
+
+// Actual DOM code
+const root1 = document.getElementById("root1");
+
+// Virtual DOM code
+const root2 = ReactDOM.createRoot(document.getElementById("root2"));
+
+setInterval(()=> {
+  root1.innerHTML = `
+<div>
+  <h1>Hello JS!</h1>
+  <input />
+  <pre>${new Date().toLocaleTimeString()}</pre>
+</div>
+`;
+
+root2.render(
+<div>
+  <h1>Hello ReactJS!</h1>
+  <input />
+  <pre>{new Date().toLocaleTimeString()}</pre>
+</div>
+);
+}, 1000);
+
+
