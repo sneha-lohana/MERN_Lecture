be43f1b2e6fea8c59872cb41a3f23f7bbd2afbc1
Sneha Lohana <sneha.lohana16@gmail.com>
2024-12-15 10:39:10 +0530

    useEffect hook solution to sideeffect

--- React-JS/React-Mini-Projects/github-cards/src/App.js
+++ React-JS/React-Mini-Projects/github-cards/src/App.js
@@ -1,14 +1,45 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import CardList from "./CardList";
 import SearchUser from "./SearchUser";
-import testData from './test-data.json';
+// import testData from './test-data.json';
+
+function fetctUsers(){
+  fetch("http://localhost:3000/test-data.json")
+  .then(res => res.json()) 
+  .then(data => console.log(data))
+}
+
+fetctUsers();
 
 function App() {
-  let [profiles, setProfile] = useState(testData);
+  let [profiles, setProfiles] = useState([]);
+
+  // function fetctUsers(){
+  //   fetch("http://localhost:3000/test-data.json")
+  //   .then(res => res.json()) 
+  //   .then(data => setProfiles(data))
+  // }
+  
+  // fetctUsers();
+
+  useEffect(()=> {
+    fetch("http://localhost:3000/test-data.json")
+    .then(res => res.json()) 
+    .then(data => setProfiles(data))
+  }, [])
+
+  const searchUsers = (username) => {
+    console.log(username);
+    const users = [].filter(profile => {
+      return profile.login.toLowerCase().includes(username.toLowerCase());
+    });
+    setProfiles(users);
+  }
+
   return (
     <div>
       <h1>Github Cards</h1>
-      <SearchUser />
+      <SearchUser onSearch={searchUsers}/>
       <CardList profiles={profiles}/>
     </div>
   );
--- React-JS/React-Mini-Projects/github-cards/src/SearchUser.js
+++ React-JS/React-Mini-Projects/github-cards/src/SearchUser.js
@@ -1,14 +1,27 @@
-import React from 'react'
+import React, { useRef, useState } from 'react'
 
-export default function SearchUser() {
+export default function SearchUser(props) {
+  // const username = useRef("Sneha");
+  const [username, setUserName] = useState("");
 
-    const handleSubmit = (event) => {
-        console.log(event);
-        event.preventDefault();
-    }
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    props.onSearch(username);
+    // console.log(event);
+    // console.log(username);
+    // console.log(username.current.value)
+  }
+  
+  const handleChange = (event) => {
+    setUserName(event.target.value);
+    // console.log(event.target.value);
+  }
+  // console.log("render");
+  
   return (
     <form onSubmit={handleSubmit}>
-        <input type='text'/>
+        {/* <input type='text' ref={username}/> */}
+        <input type='text' value={username} onChange={handleChange}/>
         <button>Search</button>
     </form>
   )
